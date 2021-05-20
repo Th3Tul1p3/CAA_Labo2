03916de0f91d14072bde71a2feda046cfc5ec1a3
jerome <jerome.arn@heig-vd.ch>
2021-05-20 23:51:38 +0200

    authentification

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,13 +1,20 @@
 use crate::argon2id13::Salt;
-use serde::Deserialize;
+use hmac::{Hmac, Mac, NewMac};
+use serde::{Deserialize, Serialize};
 use serde_json;
+use sha2::Sha256;
 use sodiumoxide::crypto::pwhash::argon2id13;
+use std::convert::TryInto;
 use ureq;
+use ureq::Error;
 
 fn main() {
-    let res = authentication();
-    let salt: UserChallenge = serde_json::from_str(&res).unwrap();
-    println!("{:?}", salt.salt);
+    let token = authentication();
+    if !token.is_empty(){
+        uplpoad(token.clone());
+        get_list(token.clone());
+        download(token.clone());
+    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -17,27 +24,69 @@ struct UserChallenge {
     salt: Salt,
 }
 
-fn authentication() -> String {
-    ureq::post("http://127.0.0.1:8080/server/jerome")
-    .set("X-My-Header", "Secret")
-    .set("Username", "jerome").call().unwrap();
-    ureq::get("http://127.0.0.1:8080/server/jerome")
+#[derive(Serialize, Deserialize, Debug)]
+struct ComputedChallenge {
+    challenge: [u8; 32],
+}
+
+fn authentication() -> String{
+    // demande du challenge au serveur
+    let res = ureq::get("http://127.0.0.1:8080/server/jerome")
         .call()
         .unwrap()
         .into_string()
-        .unwrap()
-}
-
-fn get_list(){
-    ureq::get("http://127.0.0.1:8080/server/jerome/list")
-        .call()
         .unwrap();
+
+    // désérialisation sur challenge reçu
+    let user_challenge: UserChallenge = serde_json::from_str(&res).unwrap();
+
+    // déravation de la clé à partir du sel reçu
+    let mut key = [0u8; 32];
+    argon2id13::derive_key(
+        &mut key,
+        "P@ssw0rd".as_bytes(),
+        &user_challenge.salt,
+        argon2id13::OPSLIMIT_INTERACTIVE,
+        argon2id13::MEMLIMIT_INTERACTIVE,
+    )
+    .unwrap();
+
+    // exécution du hmac de la valeur reçue
+    type HmacSha256 = Hmac<Sha256>;
+    let mut mac = HmacSha256::new_varkey(&key).expect("HMAC Error");
+    mac.update(&user_challenge.challenge.to_be_bytes());
+    let x: [u8; 32] = mac
+        .finalize()
+        .into_bytes()
+        .as_slice()
+        .try_into()
+        .expect("Wrong length");
+    let challenge = ComputedChallenge { challenge: x };
+
+    // envoi du MAC au serveur
+    match ureq::post("http://127.0.0.1:8080/server/jerome")
+        .set("Username", &user_challenge.username)
+        .send_string(&serde_json::to_string(&challenge).unwrap())
+    {
+        Ok(request) => {
+            return request.header("Token").unwrap().to_string();
+        }
+        Err(Error::Status(code, response)) => {
+            println!("{:?} {:?}", code, response);
+            return "".to_string();
+        }
+        Err(_) => {
+            return "".to_string();
+        }
+    };
 }
 
-fn uplpoad(){
-
+fn get_list(token : String) {
+    /*ureq::get("http://127.0.0.1:8080/server/jerome/list")
+    .call()
+    .unwrap();*/
 }
 
-fn download(){
+fn uplpoad(token : String) {}
 
-}
+fn download(token : String) {}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,10 @@
-use actix_web::{get, post, web, Error, HttpRequest, HttpResponse};
+use actix_web::{get, post, web/*, Error, HttpRequest*/, HttpResponse};
 use futures::StreamExt;
 //use aead::{generic_array::GenericArray, Aead, NewAead};
 //use aes_gcm::Aes256Gcm; // Or `Aes128Gcm`
-//use hmac::{Hmac, Mac, NewMac};
+use hmac::{Hmac, Mac, NewMac};
 use rand_core::{OsRng, RngCore};
-//use sha2::Sha256;
+use sha2::Sha256;
 use sodiumoxide::crypto::pwhash::argon2id13;
 //use std::fs::File;
 //use std::io::prelude::*;
@@ -13,9 +13,12 @@ use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
 use serde_json;
 use std::collections::HashMap;
+use std::convert::TryInto;
+use std::str;
+use uuid::Uuid;
 
-static mut USER_TOKEN: Vec<UserToken> = Vec::new();
-static mut USER_CHALLENGE: Vec<UserChallenge> = Vec::new();
+static mut USER_TOKEN: Vec<(String, String)> = Vec::new();
+static mut USER_CHALLENGE: Vec<(String, u64)> = Vec::new();
 
 #[derive(Debug)]
 struct User {
@@ -23,11 +26,6 @@ struct User {
     salt: Salt,
     password_kdf: [u8; 32],
     secret: String,
-}
-
-struct UserToken {
-    username: String,
-    token: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -42,13 +40,19 @@ struct SaltArgon {
     salt: Salt,
 }
 
+#[derive(Deserialize, Debug)]
+struct ComputedChallenge {
+    challenge: [u8; 32],
+}
+
 lazy_static! {
     static ref USER_DB: HashMap<&'static str, User> = {
         let mut map = HashMap::new();
-        sodiumoxide::init().unwrap();
+
+        // Cette partie se fait normalement sur le client mais est volontairement
+        // mise sur le serveur pour simplifié
         let salt = argon2id13::gen_salt();
         let mut key = [0u8; 32];
-        sodiumoxide::init().unwrap();
         argon2id13::derive_key(
             &mut key,
             "P@ssw0rd".as_bytes(),
@@ -79,6 +83,9 @@ async fn username(web::Path(user_id): web::Path<String>) -> HttpResponse {
                 salt: username.salt,
                 challenge: OsRng.next_u64(),
             };
+            unsafe {
+                USER_CHALLENGE.push((user_id, m.challenge));
+            }
             HttpResponse::Ok().body(serde_json::to_string(&m).unwrap())
         }
         None => HttpResponse::NotFound().finish(),
@@ -86,12 +93,60 @@ async fn username(web::Path(user_id): web::Path<String>) -> HttpResponse {
 }
 
 #[post("/server/{user_id}")] // <- define path parameters
-async fn username_post(web::Path(user_id): web::Path<String>) -> HttpResponse {
-    println!("Post request");
-    HttpResponse::Ok().finish()
+async fn username_post(
+    web::Path(user_id): web::Path<String>,
+    mut body: web::Payload,
+) -> HttpResponse {
+    // check dans la DB si l'utilisateur est présent
+    let user = match USER_DB.get::<str>(&user_id.to_string()) {
+        Some(user) => user,
+        None => {
+            return HttpResponse::NotFound().finish();
+        }
+    };
+
+    // lecture du body pour avoir le challenge envoyé
+    let mut bytes = web::BytesMut::new();
+    while let Some(item) = body.next().await {
+        let item = item.unwrap();
+        bytes.extend_from_slice(&item);
+    }
+
+    // on désérialise le challenge envoyé
+    let computed_challenge: ComputedChallenge =
+        serde_json::from_str(str::from_utf8(&bytes).unwrap()).unwrap();
+
+    // récupération du challenge envoyé au client
+    let challenge_to_compute: u64;
+    unsafe {
+        let index = USER_CHALLENGE.iter().position(|x| x.0 == user_id).unwrap();
+        challenge_to_compute = USER_CHALLENGE.get(index).unwrap().1;
+        USER_CHALLENGE.remove(index);
+    }
+
+    // Fait le mac à partir de la kdf dans la DB
+    type HmacSha256 = Hmac<Sha256>;
+    let mut mac = HmacSha256::new_varkey(&user.password_kdf).expect("HMAC Error");
+    mac.update(&challenge_to_compute.to_be_bytes());
+    let challenge: [u8; 32] = mac
+        .finalize()
+        .into_bytes()
+        .as_slice()
+        .try_into()
+        .expect("Wrong length");
+
+    // on teste si les valeurs sont identiques
+    if challenge == computed_challenge.challenge {
+        let user_token : String = Uuid::new_v4().hyphenated().to_string();
+        unsafe {
+            USER_TOKEN.push((user_id, user_token.clone()));
+        }
+        return HttpResponse::Ok().header("Token",user_token).finish();
+    }
+    HttpResponse::NonAuthoritativeInformation().finish()
 }
 
-#[get("/body")]
+/*#[get("/body")]
 async fn body(mut body: web::Payload, req: HttpRequest) -> Result<HttpResponse, Error> {
     let mut bytes = web::BytesMut::new();
     while let Some(item) = body.next().await {
@@ -105,14 +160,14 @@ async fn body(mut body: web::Payload, req: HttpRequest) -> Result<HttpResponse, 
         //USER_TOKEN.push("jerome".to_string());
     }
     Ok(HttpResponse::Ok().finish())
-}
+}*/
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Le serveur est prêt à recevoir des requêtes");
     use actix_web::{App, HttpServer};
 
-    HttpServer::new(|| App::new().service(username).service(body).service(username_post))
+    HttpServer::new(|| App::new().service(username).service(username_post))
         .bind("127.0.0.1:8080")?
         .run()
         .await
