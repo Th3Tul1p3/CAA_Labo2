86f07f8d7deec8ca3b9eb4fb7545312e3130e60b
jerome <jerome.arn@heig-vd.ch>
2021-05-24 07:14:39 +0200

    finit, -> refactor

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,6 +2,8 @@ use crate::argon2id13::Salt;
 use aead::{generic_array::GenericArray, Aead, NewAead};
 use aes_gcm::Aes256Gcm;
 use ecies::{decrypt, encrypt, utils::generate_keypair, PublicKey, SecretKey};
+
+use generic_array::typenum::{UInt, UTerm, B0, B1};
 use hmac::{Hmac, Mac, NewMac};
 use rand_core::{OsRng, RngCore};
 use serde::{Deserialize, Serialize};
@@ -10,7 +12,6 @@ use sha2::Sha256;
 use sodiumoxide::crypto::pwhash::argon2id13;
 use std::convert::TryInto;
 use std::env;
-use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 use std::str;
@@ -24,7 +25,7 @@ fn main() {
     if !token.is_empty() {
         uplpoad(token.clone(), pk, "shadow".to_string());
         get_list(token.clone());
-        download(token.clone(), "shadow".to_string());
+        download(token.clone(), "shadow".to_string(), sk);
     }
 }
 
@@ -101,17 +102,20 @@ fn authentication() -> String {
 }
 
 fn get_list(token: String) {
-    match ureq::get("http://127.0.0.1:8080/download")
-        .set("Username", "jerome")
+    let resp = ureq::get("http://127.0.0.1:8080/list")
         .set("Token", &token)
+        .set("Username", "jerome")
         .call()
-    {
-        Ok(_) => {}
-        Err(Error::Status(code, response)) => {
-            println!("{:?} {:?}", code, response);
-        }
-        Err(_) => {}
-    };
+        .unwrap()
+        .into_string()
+        .unwrap();
+
+    // désérialisation sur challenge reçu
+    let list: Vec<String> = serde_json::from_str(&resp).unwrap();
+    println!(
+        "Voici la liste des fichiers auxquelles vous avez accès: {:?}",
+        list
+    );
 }
 
 fn uplpoad(token: String, pub_key: PublicKey, file_name: String) -> bool {
@@ -142,7 +146,7 @@ fn uplpoad(token: String, pub_key: PublicKey, file_name: String) -> bool {
     OsRng.fill_bytes(&mut nonce);
     let nonce_aes = GenericArray::from_slice(&nonce);
 
-    // chiffrement AES-GCM
+    // chiffrement des données AES-GCM
     let ciphertext = aead
         .encrypt(nonce_aes, contents.as_bytes().as_ref())
         .expect("encryption failure!");
@@ -160,6 +164,9 @@ fn uplpoad(token: String, pub_key: PublicKey, file_name: String) -> bool {
         key: _encrypted_key,
     };
 
+    let mut file = File::create("cipher").unwrap();
+    file.write_all(&ciphertext).unwrap();
+
     let mut metada_file = file_name.to_owned();
     metada_file.push_str(".metadata");
     let metadata_file = env::current_dir().unwrap().join(&metada_file);
@@ -171,21 +178,21 @@ fn uplpoad(token: String, pub_key: PublicKey, file_name: String) -> bool {
     match ureq::post("http://127.0.0.1:8080/upload")
         .set("FileName", &file_name)
         .set("Token", &token)
+        .set("Username", "jerome")
         .send_bytes(&ciphertext)
     {
-        Ok(_) => {
-        }
+        Ok(_) => {}
         Err(Error::Status(code, response)) => {
             println!("{:?} {:?}", code, response);
         }
-        Err(_) => {
-        }
+        Err(_) => {}
     };
 
-    // mettre le chiffré dans le body et envoyer avec le token et le nom de fichier
+    // mettre le fichier metadata dans le body et envoyer avec le token et le nom de fichier
     match ureq::post("http://127.0.0.1:8080/upload")
         .set("FileName", &metada_file)
         .set("Token", &token)
+        .set("Username", "jerome")
         .send_string(&serialized_metadata)
     {
         Ok(_) => {
@@ -201,21 +208,52 @@ fn uplpoad(token: String, pub_key: PublicKey, file_name: String) -> bool {
     };
 }
 
-fn download(token: String, file_name: String) -> bool {
-    match ureq::get("http://127.0.0.1:8080/download")
+fn download(token: String, file_name: String, sk: SecretKey) {
+    // téléchargé le chiffré
+    let mut cipher_stream = ureq::get("http://127.0.0.1:8080/download")
         .set("FileName", &file_name)
         .set("Token", &token)
+        .set("Username", "jerome")
         .call()
-    {
-        Ok(_) => {
-            return true;
-        }
-        Err(Error::Status(code, response)) => {
-            println!("{:?} {:?}", code, response);
-            return false;
-        }
-        Err(_) => {
-            return false;
-        }
-    };
+        .unwrap()
+        .into_reader();
+    let mut cipher: Vec<u8> = Vec::new();
+    cipher_stream.read_to_end(&mut cipher).unwrap();
+
+    // téléchargé les métadonnées
+    let mut metada_file = file_name.to_owned();
+    metada_file.push_str(".metadata");
+    let resp = ureq::get("http://127.0.0.1:8080/download")
+        .set("FileName", &metada_file)
+        .set("Token", &token)
+        .set("Username", "jerome")
+        .call()
+        .unwrap()
+        .into_string()
+        .unwrap();
+
+    // déchiffrer la clés
+    let metadata: Metadata = serde_json::from_str(&resp).unwrap();
+    let decrypted_key: Vec<u8> = decrypt(&sk.serialize(), &metadata.key).unwrap();
+
+    // déchiffrer les données
+    // préparation des clés pour AES-GCM et du nonce
+    let key_aes = GenericArray::clone_from_slice(&decrypted_key);
+    let aead = Aes256Gcm::new(key_aes);
+    let nonce_aes: aead::generic_array::GenericArray<
+        u8,
+        UInt<UInt<UInt<UInt<UTerm, B1>, B1>, B0>, B0>,
+    > = *GenericArray::from_slice(&metadata.nonce);
+
+    // chiffrement des données AES-GCM
+    let plaintext = aead
+        .decrypt(&nonce_aes, cipher.as_ref())
+        .expect("encryption failure!");
+    let string: String = String::from_utf8_lossy(&plaintext).to_string();
+
+    println!("{:?}", String::from_utf8_lossy(&plaintext));
+    // écrire dans un fichier
+
+    let mut file = File::create("filename").unwrap();
+    file.write_all(&string.as_bytes()).unwrap();
 }
